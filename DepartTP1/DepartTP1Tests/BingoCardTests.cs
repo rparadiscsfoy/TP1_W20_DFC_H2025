4ea9d1c436c5644171390b5df9d2ecfc13c8a0e1
Raphael Paradis <rparadis@csfoy.ca>
2025-02-06 11:24:29 -0500

    Version initiale

---
+++ DepartTP1/DepartTP1Tests/BingoCardTests.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SolutionTP1;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SolutionTP1.Tests
+{
+    [TestClass()]
+    public class BingoCardTests
+    {
+        private void MakeSureThisMethodCompiles()
+        {
+            NumberGenerator generator = new NumberGenerator();
+            BingoCard card = new BingoCard(5, generator);
+            card.MarkNumber(3);
+            int score = card.ComputeScore();
+        }
+
+        /*
+         * Placez vos tests ici
+         */
+    }
+}
